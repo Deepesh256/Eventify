7fb393c0f7f33a7aac6e2324dda37aef1b695f15
Deepesh <deepeshkumar6300@gmail.com>
2026-09-09 23:00:45 +0530

    Update CORS for new Eventify domain

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://eventify-gray.vercel.app",
+  "https://eventifybydeepesh.vercel.app",
 ];
 app.use(
   cors({
